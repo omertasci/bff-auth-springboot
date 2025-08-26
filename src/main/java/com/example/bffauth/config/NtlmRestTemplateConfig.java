18545fb9ef8fb0d91377522eb9adb97eb42ed9af
omertasci <omertasci.ce@gmail.com>
2025-08-26 12:45:00 +0300

    ag sharepoint ntlm integration yml file usage -2

--- src/main/java/com/example/bffauth/config/NtlmRestTemplateConfig.java
+++ src/main/java/com/example/bffauth/config/NtlmRestTemplateConfig.java
@@ -6,29 +6,30 @@ import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+    @Configuration
     public class NtlmRestTemplateConfig {
 
         @Autowired
         private Environment env;
 
-       /* @Value("${sharepoint.ag.host}")
+        @Value("${sharepoint.ag.host}")
         String host;
 
         @Value("${sharepoint.ag.port}")
-        int port;*/
+        int port;
 
         @Bean
         public RestTemplate restTemplate() {
             BasicCredentialsProvider credsProvider = new BasicCredentialsProvider();
             credsProvider.setCredentials(
-                    new AuthScope(env.getProperty("sharepoint.ag.host"), Integer.parseInt(env.getProperty("sharepoint.ag.port"))),
+                    new AuthScope(host, port),
                     new NTCredentials("**username**", "**password**".toCharArray(), "", "**domain**")
             );
 
