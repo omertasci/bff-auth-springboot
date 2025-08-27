9c1cf6188f1b57a310a44a7e130936c571dbbd1b
Veys101 <vys2626@gmail.com>
2025-08-28 01:12:42 +0300

    refactoring

--- src/main/java/com/example/bffauth/config/NtlmRestTemplateConfig.java
+++ src/main/java/com/example/bffauth/config/NtlmRestTemplateConfig.java
@@ -5,11 +5,9 @@ import org.apache.hc.client5.http.auth.NTCredentials;
 import org.apache.hc.client5.http.impl.auth.BasicCredentialsProvider;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
